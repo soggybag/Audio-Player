5c445a44579d89f3261177e7156df22e4fc2949a
Mitchell Hudson <soggybag@gmail.com>
2016-04-08 07:16:10 -0700

    Initial Commit

---
+++ Audio-Player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Audio-Player
+//
+//  Created by mitchell hudson on 4/8/16.
+//  Copyright © 2016 mitchell hudson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
